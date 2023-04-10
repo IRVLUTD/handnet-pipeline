b419d3a77e73575c8f75109a531e470cc2749906
Shaurya Dwivedi <67282772+Shaurya0108@users.noreply.github.com>
2023-04-09 23:03:10 -0500

    Update detectHandImage.py

--- detectHandImage.py
+++ detectHandImage.py
@@ -17,3 +17,21 @@ for filename in glob.glob('../Downloads/imageDataset/0104T143721/*.png'): #repla
 
 #calls the run_network() from ros_demo.py
 ros_demo.ImageListener.run_network()
+
+# import glob
+# import cv2
+
+## create a list of image paths
+# image_paths = glob.glob('../Downloads/imageDataset/0104T143721/*.jpg')
+
+## create an instance of the class containing the run_network function
+# network_runner = NetworkRunner()
+
+## loop through each image and pass it to the run_network function
+# for image_path in image_paths:
+#     # read the image
+#     im = cv2.imread(image_path)
+
+#    # pass the image to the run_network function
+#     network_runner.run_network(im)
+
