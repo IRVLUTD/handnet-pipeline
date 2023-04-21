9ea6f2306b9bc482e84eaf2a752b08e950f1df3e
Neil Song <song.neil.song@gmail.com>
2023-04-20 21:02:45 -0500

    Update Pose2Mesh pretrained weights gdrive ID

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -5,4 +5,4 @@ wget https://utdallas.box.com/shared/static/xgusayqw8xgmxnfzzqi1r0hvrxukknzo.pth
 wget https://utdallas.box.com/shared/static/s6igmdvxsis8sib101xko7o44gwompgz.pth -O "models/fcos.pth"
 mkdir experiment
 mkdir experiment/pose2mesh_manoJ_train_freihand
-wget --no-check-certificate -r "https://drive.google.com/uc?export=download&id=1Ub5WtF5oesIzGPLTLaNSN2TxHJDjYuX2&confirm=t" -O "experiment/pose2mesh_manoJ_train_freihand/final.pth.tar" 
+wget --no-check-certificate -r "https://drive.google.com/uc?export=download&id=1JezEmWwX_xE61HasRj313ufGSL9PvqIp&confirm=t" -O "experiment/pose2mesh_manoJ_train_freihand/final.pth.tar" 
